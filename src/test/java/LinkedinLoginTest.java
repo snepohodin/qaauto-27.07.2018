04ef28c58dc9b5949eb875bf84abf6a4779a5491
snepohodin <rodman4ik@gmail.com>
2018-08-06 00:19:48 +0300

    Lesson 3 homework

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -10,7 +10,29 @@ public class LinkedinLoginTest {
 
     @Test
     public void successfulLoginTest () {
+        WebDriver browser = new FirefoxDriver();
+        // maximize the browser window
+        browser.manage().window().maximize();
+        browser.get("https://www.linkedin.com/");
+        WebElement userEmailField = browser.findElement(By.xpath("//input[@id ='login-email']"));
+        WebElement userPasswordField = browser.findElement(By.xpath("//input[@id='login-password']"));
+        WebElement signInButton = browser.findElement(By.xpath("//input[@id='login-submit']"));
 
+        userEmailField.sendKeys("rdmntest@gmail.com");
+        userPasswordField.sendKeys("July222@");
+        signInButton.click();
+
+        //url verification
+        String url = browser.getCurrentUrl();
+        Assert.assertEquals(url, "https://www.linkedin.com/feed/" ,"Invalid URL");
+
+        //title verification
+        String title = browser.getTitle();
+        Assert.assertEquals(title,"LinkedIn", "Invalid title");
+
+        //upgrade button verification
+        WebElement upgradeButton = browser.findElement(By.xpath("//a[@data-control-name='premium_nav_upsell_text_click']"));
+        Assert.assertEquals(upgradeButton.getText(), "Free Upgrade to Premium","Alert box has incorrect message.");
     }
 
     @Test
