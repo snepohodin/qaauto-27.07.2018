c3c14a33c3d9675b0b876e921e0d4f40943297d2
snepohodin <rodman4ik@gmail.com>
2018-09-07 19:11:05 +0300

    Lesson 11 hometask testng.xml v2

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -32,48 +32,6 @@ public class BaseTest {
         linkedinLoginPage = new LinkedinLoginPage(browser);
     }
 
-//    @Parameters({"browserName", "envURL"})
-//    @BeforeMethod
-//    public void beforeMethod(@Optional("firefox") String browserName,@Optional("ru.linkedin.com") String environmentUrl) {
-//        if (browserName.toLowerCase().equals("firefox")){
-//            browser = new FirefoxDriver();
-//        }
-//        if (browserName.toLowerCase().equals("chrome")){
-//            browser = new ChromeDriver();
-//        } else {
-//            try {
-//                throw new Exception("browserName "+browserName+"is not supported.");
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        browser.get("https://www.linkedin.com/");
-//        linkedinLoginPage = new LinkedinLoginPage(browser);
-//    }
-
-    @Parameters("envURL")
-    public void beforeMethod2(@Optional("linkedin.com") String envURL) {
-        if (envURL.toLowerCase().equals("ua.linkedin.com")){
-            browser = new FirefoxDriver();
-            browser.get("ua.linkedin.com");
-        }
-        if (envURL.toLowerCase().equals("ru.linkedin.com")){
-            browser = new FirefoxDriver();
-            browser.get("ru.linkedin.com");
-        }
-        if (envURL.toLowerCase().equals("de.linkedin.com")){
-            browser = new FirefoxDriver();
-            browser.get("de.linkedin.com");
-        } else {
-            try {
-                throw new Exception("URL "+envURL+" incorrect.");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        linkedinLoginPage = new LinkedinLoginPage(browser);
-    }
-
     @AfterMethod
     public void afterMethod() {
         browser.close();
